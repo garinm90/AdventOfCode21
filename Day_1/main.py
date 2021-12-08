f9f77bd2dec88551e91c870f3f8a1eb3945c51aa
Max garin <maxgarin@gmail.com>
2021-12-07 21:58:38 -0500

    Part Two

--- Day_1/main.py
+++ Day_1/main.py
@@ -13,3 +13,19 @@ for value in inputs:
     last_value = value
 
 print(number_of_increase)
+
+
+number_of_increase = 0
+last_value = 0
+for i in range(len(inputs)):
+    if len(inputs) - i < 3:
+        break
+    set_of_3 = inputs[i:i+3]
+    total = sum(set_of_3)
+    if i == 0 :
+        continue
+    if total > last_value:
+        number_of_increase += 1
+    last_value = total
+    
+print(number_of_increase)
